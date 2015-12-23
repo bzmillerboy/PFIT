d81d01b02e750ef751fbd9f049d5074553526869
Ryan Beljan <ryan@805creative.com>
2015-12-23 17:16:07 -0500

    text updates

--- page-advisory.php
+++ page-advisory.php
@@ -23,9 +23,9 @@ get_header(); ?>
     <section class="hero advisory">
         <div class="container">
             <h1 class="brand">
-                We don’t mind getting our hands dirty
+                We love getting our hands dirty.
             </h1>
-            <p>We’re not just healthcare consultants, and we’re not just IT people. We give intelligent, comprehensive solutions, but more importantly, we know how to execute.  Whether working alongside an IT department or managing the whole process, we are used to getting our hands dirty.</p>
+            
         </div>
     </section>
 
@@ -33,8 +33,8 @@ get_header(); ?>
         <div class="container">
             <div class="row">
                 <div class="col-sm-8 col-sm-offset-2 text-center">
-                    <h3>EXPERTISE AND GUIDANCE FOR TODAY’S TOUGHEST HEALTHCARE CHALLENGES</h3>
-					<p class="text-muted">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.</p>
+                    <h3>EXPERTISE AND GUIDANCE FOR TODAY’S HEALTHCARE CHALLENGES.</h3>
+					<p class="text-muted">We’re not just healthcare consultants, and we’re not just IT people. We provide intelligent, comprehensive solutions, and more importantly, we know how to execute them. Whether working alongside an IT department or managing the whole process, we’re comfortable rolling up our sleeves and getting our hands dirty.</p>
                 </div>
             </div>
         </div>
@@ -56,14 +56,14 @@ get_header(); ?>
                 <div class="col-sm-12 col-md-4">
                     <a class="serv-off-title collapsed" href="#collapseOff1" data-toggle="collapse">
                         <h4 class="serv-off-title-text">
-                            Strategic and Economic Planning
+                            Patient-Centered Medical Home
                             <svg class="icon icon-transparent" xmlns:xlink="http://www.w3.org/1999/xlink">
                               <use class="serv-off-arrow" xlink:href="#icon-arrow-right" x="0" y="0" />
                             </svg>
                         </h4>
                     </a>
                     <div class="collapse" id="collapseOff1">
-                        <p class="serv-off-desc">To achieve the results you expect, your IT function needs to be driven by your business and clinical objectives. We work with you to validate your objectives, match up appropriate IT solutions, and create a plan for action. </p>
+                        <p class="serv-off-desc">Becoming a recognized Patient-Centered Medical Home (PCMH) can be overwhelming. Our proven roadmap to success breaks the PCMH quest into manageable pieces and brings clarity to the process, while our practice-coaching model gives clinicians and leadership the tools and confidence they need to reach Level 3.</p>
                     </div>
 
                     <a class="serv-off-title collapsed" href="#collapseOff4" data-toggle="collapse">
@@ -75,47 +75,10 @@ get_header(); ?>
                         </h4>
                     </a>
                     <div class="collapse" id="collapseOff4">
-                        <p class="serv-off-desc">To achieve the results you expect, your IT function needs to be driven by your business and clinical objectives. We work with you to validate your objectives, match up appropriate IT solutions, and create a plan for action. </p>
+                        <p class="serv-off-desc">Building a successful organization doesn’t just require making changes—it also requires sustaining them. Our practice transformation experts use data to help identify opportunities for improvement. Then we’ll coach you through the process step by step to create performance improvement that lasts.</p>
                     </div>
 
                     <a class="serv-off-title collapsed" href="#collapseOff7" data-toggle="collapse">
-                        <h4 class="serv-off-title-text">
-                            Meaningful Use
-                            <svg class="icon icon-transparent" xmlns:xlink="http://www.w3.org/1999/xlink">
-                              <use class="serv-off-arrow" xlink:href="#icon-arrow-right" x="0" y="0" />
-                            </svg>
-                        </h4>
-                    </a>
-                    <div class="collapse" id="collapseOff7">
-                        <p class="serv-off-desc">To achieve the results you expect, your IT function needs to be driven by your business and clinical objectives. We work with you to validate your objectives, match up appropriate IT solutions, and create a plan for action. </p>
-                    </div>
-
-                    <a class="serv-off-title collapsed" href="#collapseOff10" data-toggle="collapse">
-                        <h4 class="serv-off-title-text">
-                            Assessments
-                            <svg class="icon icon-transparent" xmlns:xlink="http://www.w3.org/1999/xlink">
-                              <use class="serv-off-arrow" xlink:href="#icon-arrow-right" x="0" y="0" />
-                            </svg>
-                        </h4>
-                    </a>
-                    <div class="collapse" id="collapseOff10">
-                        <p class="serv-off-desc">To achieve the results you expect, your IT function needs to be driven by your business and clinical objectives. We work with you to validate your objectives, match up appropriate IT solutions, and create a plan for action. </p>
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-4">
-                    <a class="serv-off-title collapsed" href="#collapseOff2" data-toggle="collapse">
-                        <h4 class="serv-off-title-text">
-                            Accountable Care Solutions
-                            <svg class="icon icon-transparent" xmlns:xlink="http://www.w3.org/1999/xlink">
-                              <use class="serv-off-arrow" xlink:href="#icon-arrow-right" x="0" y="0" />
-                            </svg>
-                        </h4>
-                    </a>
-                    <div class="collapse" id="collapseOff2">
-                        <p class="serv-off-desc">To achieve the results you expect, your IT function needs to be driven by your business and clinical objectives. We work with you to validate your objectives, match up appropriate IT solutions, and create a plan for action. </p>
-                    </div>
-
-                    <a class="serv-off-title collapsed" href="#collapseOff5" data-toggle="collapse">
                         <h4 class="serv-off-title-text">
                             Population Health
                             <svg class="icon icon-transparent" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -123,8 +86,45 @@ get_header(); ?>
                             </svg>
                         </h4>
                     </a>
+                    <div class="collapse" id="collapseOff7">
+                        <p class="serv-off-desc">Managing the health of your patients has never been more important, or more complex. We teach our customers how to develop actionable performance data and identify high-risk patients. Then we help develop meaningful interventions and techniques to improve patients’ health, whether they’re in your office or at home.</p>
+                    </div>
+
+                    <a class="serv-off-title collapsed" href="#collapseOff10" data-toggle="collapse">
+                        <h4 class="serv-off-title-text">
+                            Informatics & Technology
+                            <svg class="icon icon-transparent" xmlns:xlink="http://www.w3.org/1999/xlink">
+                              <use class="serv-off-arrow" xlink:href="#icon-arrow-right" x="0" y="0" />
+                            </svg>
+                        </h4>
+                    </a>
+                    <div class="collapse" id="collapseOff10">
+                        <p class="serv-off-desc">Often clients need an external doublecheck of their current IT state. With a strong staff of professionals from a wide range of IT backgrounds, from interface design to network engineering to hardware management, we can be your second set of eyes. Our IT staff will help you confirm your existing IT infrastructure and work with you to develop your long-term strategy.</p>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <a class="serv-off-title collapsed" href="#collapseOff2" data-toggle="collapse">
+                        <h4 class="serv-off-title-text">
+                            Integration Services
+                            <svg class="icon icon-transparent" xmlns:xlink="http://www.w3.org/1999/xlink">
+                              <use class="serv-off-arrow" xlink:href="#icon-arrow-right" x="0" y="0" />
+                            </svg>
+                        </h4>
+                    </a>
+                    <div class="collapse" id="collapseOff2">
+                        <p class="serv-off-desc">From EHR to Practice Management software—and imaging systems to Health Information Exchange platforms—today’s healthcare environment demands a complex clinical information system that should be comprehensively integrated for maximum effectiveness and efficiency.</p>
+                    </div>
+
+                    <a class="serv-off-title collapsed" href="#collapseOff5" data-toggle="collapse">
+                        <h4 class="serv-off-title-text">
+                            Revenue Cycle Consulting
+                            <svg class="icon icon-transparent" xmlns:xlink="http://www.w3.org/1999/xlink">
+                              <use class="serv-off-arrow" xlink:href="#icon-arrow-right" x="0" y="0" />
+                            </svg>
+                        </h4>
+                    </a>
                     <div class="collapse" id="collapseOff5">
-                        <p class="serv-off-desc">To achieve the results you expect, your IT function needs to be driven by your business and clinical objectives. We work with you to validate your objectives, match up appropriate IT solutions, and create a plan for action. </p>
+                        <p class="serv-off-desc">Receiving appropriate payment for care rendered is no easy task. Billing changes are a driving force of change in the medical industry, and our consultants help you stay ahead of the game by ensuring that your company is generating the appropriate revenue for the medical services you provide.</p>
                     </div>
 
                     <a class="serv-off-title collapsed" href="#collapseOff9" data-toggle="collapse">
@@ -141,55 +141,43 @@ get_header(); ?>
 
                     <a class="serv-off-title collapsed" href="#collapseOff12" data-toggle="collapse">
                         <h4 class="serv-off-title-text">
-                            Patient Engagement
+                            Strategic and Economic Planning
                             <svg class="icon icon-transparent" xmlns:xlink="http://www.w3.org/1999/xlink">
                               <use class="serv-off-arrow" xlink:href="#icon-arrow-right" x="0" y="0" />
                             </svg>
                         </h4>
                     </a>
                     <div class="collapse" id="collapseOff12">
-                        <p class="serv-off-desc">To achieve the results you expect, your IT function needs to be driven by your business and clinical objectives. We work with you to validate your objectives, match up appropriate IT solutions, and create a plan for action. </p>
+                        <p class="serv-off-desc">To achieve the results you expect, your IT function needs to be driven by your business and clinical objectives. We work with you to validate your objectives, match appropriate IT solutions, and create a plan for action.</p>
                     </div>
 
                 </div>
                 <div class="col-sm-12 col-md-4">
                     <a class="serv-off-title collapsed" href="#collapseOff3" data-toggle="collapse">
                         <h4 class="serv-off-title-text">
-                            Patient Centered Medical Home
+                            Practice Analytics
                             <svg class="icon icon-transparent" xmlns:xlink="http://www.w3.org/1999/xlink">
                               <use class="serv-off-arrow" xlink:href="#icon-arrow-right" x="0" y="0" />
                             </svg>
                         </h4>
                     </a>
                     <div class="collapse" id="collapseOff3">
-                        <p class="serv-off-desc">To achieve the results you expect, your IT function needs to be driven by your business and clinical objectives. We work with you to validate your objectives, match up appropriate IT solutions, and create a plan for action. </p>
+                        <p class="serv-off-desc">Every other business in America runs their business on data—healthcare should, too. Contact us to find out how to leverage your data to perform more profitably.</p>
                     </div>
 
                     <a class="serv-off-title collapsed" href="#collapseOff6" data-toggle="collapse">
                         <h4 class="serv-off-title-text">
-                            Informatics and Technology
+                            Meaningful Use
                             <svg class="icon icon-transparent" xmlns:xlink="http://www.w3.org/1999/xlink">
                               <use class="serv-off-arrow" xlink:href="#icon-arrow-right" x="0" y="0" />
                             </svg>
                         </h4>
                     </a>
                     <div class="collapse" id="collapseOff6">
-                        <p class="serv-off-desc">To achieve the results you expect, your IT function needs to be driven by your business and clinical objectives. We work with you to validate your objectives, match up appropriate IT solutions, and create a plan for action. </p>
+                        <p class="serv-off-desc">From planning to attestation to audit, let us help maximize the return on your EHR investment.</p>
                     </div>
 
                     <a class="serv-off-title collapsed" href="#collapseOff8" data-toggle="collapse">
-                        <h4 class="serv-off-title-text">
-                            Integration Services
-                            <svg class="icon icon-transparent" xmlns:xlink="http://www.w3.org/1999/xlink">
-                              <use class="serv-off-arrow" xlink:href="#icon-arrow-right" x="0" y="0" />
-                            </svg>
-                        </h4>
-                    </a>
-                    <div class="collapse" id="collapseOff8">
-                        <p class="serv-off-desc">To achieve the results you expect, your IT function needs to be driven by your business and clinical objectives. We work with you to validate your objectives, match up appropriate IT solutions, and create a plan for action. </p>
-                    </div>
-
-                    <a class="serv-off-title collapsed" href="#collapseOff11" data-toggle="collapse">
                         <h4 class="serv-off-title-text">
                             Governance and Operations
                             <svg class="icon icon-transparent" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -197,8 +185,20 @@ get_header(); ?>
                             </svg>
                         </h4>
                     </a>
+                    <div class="collapse" id="collapseOff8">
+                        <p class="serv-off-desc">Healthy governance is critical to obtaining proper results and returns on Information Technology. We can help define the decisions that need to be made to craft a governance program that’s streamlined and effective.</p>
+                    </div>
+
+                    <a class="serv-off-title collapsed" href="#collapseOff11" data-toggle="collapse">
+                        <h4 class="serv-off-title-text">
+                            Patient Engagement & Satisfaction
+                            <svg class="icon icon-transparent" xmlns:xlink="http://www.w3.org/1999/xlink">
+                              <use class="serv-off-arrow" xlink:href="#icon-arrow-right" x="0" y="0" />
+                            </svg>
+                        </h4>
+                    </a>
                     <div class="collapse" id="collapseOff11">
-                        <p class="serv-off-desc">To achieve the results you expect, your IT function needs to be driven by your business and clinical objectives. We work with you to validate your objectives, match up appropriate IT solutions, and create a plan for action. </p>
+                        <p class="serv-off-desc">We’ve got the experience and systems to implement empirically based survey tools to help you understand what your patients really think about your practice.</p>
                     </div>
                 </div>
             </div>
