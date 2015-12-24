affe4c9cbdc3c90773cb294f424e8c129e0975aa
Ryan Beljan <ryan@805creative.com>
2015-12-24 09:28:58 -0500

    arrows

--- page-compliance-and-security.php
+++ page-compliance-and-security.php
@@ -112,7 +112,7 @@ shepherd guide.</p>
         </div>
     </section>
     
-    <section class="section pad-section">
+    <section class="section pad-section section-arrows">
         <div class="container">
             <div class="row">
                 <div class="col-sm-8 col-sm-offset-2 text-center">
