61aaa227d5a35edac621d3b1452e39b8c1722633
Ryan Beljan <ryan@805creative.com>
2015-12-30 16:41:33 -0500

    header

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
 <link rel="shortcut icon" href="<?php bloginfo( 'template_directory' ); ?>/favicon.ico">
 
 <?php wp_head(); ?>
-<script src="<?php echo file_get_contents( get_template_directory_uri() . '/js/html5shiv.min.js' ); ?>"></script>
+
 </head>
 
 <body <?php body_class(); ?>>
