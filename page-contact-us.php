96a24f9103fefe3c7da6b20ee8f68c56065c6c33
Brian Miller <bzmiller82@gmail.com>
2015-12-30 11:20:13 -0500

    Contact Us Styles and Fixes

--- page-contact-us.php
+++ page-contact-us.php
@@ -78,7 +78,7 @@ get_header(); ?>
             <div class="row">
                 <div class="col-sm-12">
                     <div class="row">
-                        <?php echo do_shortcode( '[contact-form-7 id="4" title="Contact Us"]' ); ?>
+                        <?php echo do_shortcode( '[contact-form-7 id="106" title="Contact Us"]' ); ?>
                     </div>
                </div>
            </div>
