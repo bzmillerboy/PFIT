88a729deda81ea7475ce50ba3f163ce42a396732
Ryan Beljan <ryan@805creative.com>
2016-01-22 11:20:55 -0500

    add page

---
+++ page-news-signup.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package pfit
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+<div class="wrapper">
+
+    <section class="hero careers" style="background-attachment:inherit;">
+        <div class="container">
+			<h1 class="brand">
+                Stay Updated.
+            </h1>
+
+        </div>
+    </section>
+
+    <section class="section pad-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2 text-center">
+                    <h3>SIGN UP TO RECEIVE NEWS</h3>
+					<p class="text-muted">Use the form below to signup to receive period news updates from Path Forward.</p>
+                    
+                    <iframe src="https://campaigndirector.myconnectwise.net/serv/form/564b75757a3e9a552c5f8b2d/56a24a1062009c7f51a4cd78/jsweeney2" frameborder=0 scrolling="no" width="650px" height="600px"></iframe>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
+
+    
+
+    		</main><!-- #main -->
+    	</div><!-- #primary -->
+<?php get_footer(); ?>
